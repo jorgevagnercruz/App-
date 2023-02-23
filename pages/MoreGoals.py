ce0083429058467ff6ce1a1a33d21ca8870b0ecc
jorgevagnercruz <125403404+jorgevagnercruz@users.noreply.github.com>
2023-02-23 10:00:47 -0300

    Update MoreGoals.py

--- pages/MoreGoals.py
+++ pages/MoreGoals.py
@@ -1,6 +1,29 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import datetime
+from datetime import date
+
+st.title("Times com mais gols em casa")
+
+dia = st.date_input(
+    "Data de Análise",
+    date.today())
+
+def load_data_jogos():
+    data_jogos = pd.read_excel("https://www.football-data.co.uk/new/BRA.xlsx")
+    
+    return data_jogos
+
+df_jogos = load_data_jogos()
+
+st.dataframe(df_jogos)
+
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
 import base64
 
 st.title("Web App Football Data")
