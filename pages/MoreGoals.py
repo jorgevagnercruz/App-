0f6eca41a65e9a749388ba6007df3c6457aea761
jorgevagnercruz <125403404+jorgevagnercruz@users.noreply.github.com>
2023-02-23 10:25:19 -0300

    Update MoreGoals.py

--- pages/MoreGoals.py
+++ pages/MoreGoals.py
@@ -14,28 +14,3 @@ def load_data_jogos():
 df_jogos = load_data_jogos()
 
 st.dataframe(df_jogos)
-
-df = df_jogos
-df = df[['Season','Date','Home','Away','HG','AG','Res','PH','PD','PA']]
-df.columns = ['Season','Date','Home','Away','Goals_H','Goals_A','Result','Odds_H','Odds_D','Odds_A']
-
-flt = df.Season == 2022
-df = df[flt]
-
-def Prob_Home():
-    P_H = 1/df.Odds_H
-    return P_H
-
-def Prob_Draw():
-    P_D = 1/df.Odds_D
-    return P_D
-
-def Prob_Away():
-    P_A = 1/df.Odds_A
-    return P_A
-  
-df['p(H)'] = Prob_Home()
-df['p(D)'] = Prob_Draw()
-df['p(A)'] = Prob_Away()
-
-df
